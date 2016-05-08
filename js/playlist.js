cda697b6691ae69ef6acd097813201f7a8268a04
Chris Gaona <thebeloved88@sbcglobal.net>
2016-05-08 13:41:25 -0700

    Adds initial programming to playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -1,12 +1,38 @@
 //create playlist object
-
+var Playlist = function() {
+  this.songs = [];
+  this.nowPlayingIndex = 0;
+}
 
 //add songs to playlist
+Playlist.prototype.add = function (song) {
+  this.songs.push(song);
+};
 
 //play song
+Playlist.prototype.play = function () {
+  var currentSong = this.songs[this.nowPlayingIndex];
+  currentSong.play();
+};
 
 //stop song
+Playlist.prototype.stop = function () {
+  var currentSong = this.songs[this.nowPlayingIndex];
+  currentSong.stop();
+};
 
 //next song
+Playlist.prototype.next = function () {
+  this.stop();
+  this.nowPlayingIndex++;
+  if (this.nowPlayingIndex >= this.songs.length) {
+    this.nowPlayingIndex = 0;
+  }
+  this.play();
+};
 
 //render html string in html page
+Playlist.prototype.renderInElement = function (element) {
+  element.innerHTML = this.songs[this.nowPlayingIndex].toHTML();
+
+};
